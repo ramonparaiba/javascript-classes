5bed6f9c8c964a15d5d4fd303c85cb981d8a2257
Pablo Ramon <ramonparaiba@gmail.com>
2022-09-30 23:34:06 -0300

    Project: Contadorzin

--- Miniprojetos JS/005 - Contador/script.js
+++ Miniprojetos JS/005 - Contador/script.js
@@ -7,12 +7,27 @@ let contInt = parseInt(contador.innerHTML)
 decrem.addEventListener('click', () => {
   contInt--
   contador.innerHTML = contInt
+  colorCounter()
+  
 })
 reseta.addEventListener('click', () => {
   contInt = 0
   contador.innerHTML = 0
+  contador.style.color = "#f5f5f5";
+  colorCounter()  
 })
 incrementa.addEventListener('click', () => {
   contInt++
   contador.innerHTML = contInt
+  contador.style.color = "#05c46b";
+  colorCounter()
 })
+
+function colorCounter(){
+  if(contInt < 0)
+  contador.style.color = "red";
+  else if(contInt == 0)
+    contador.style.color = "#f5f5f5";
+    else
+      contador.style.color = "#3c40c6";
+}
